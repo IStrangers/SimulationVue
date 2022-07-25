8539e36c20ddb4a27088c74909ee38b4b3ff6924
qqq000qqq@aliyun.com <xqq1258460261>
2022-07-25 15:30:05 +0800

    20220725

--- packages/runtime-core/src/components/KeepAlive.ts
+++ packages/runtime-core/src/components/KeepAlive.ts
@@ -9,10 +9,12 @@ const enum KeepAliveTagAttr {
 
 class KeepAliveImpl {
   __isKeepAlive__ = true
-  props = {
-    include: {},
-    exclude: {}
+  props : any = {
+    include: [],
+    exclude: [],
+    max: undefined
   }
+  keys = new Set()
   cache = new Map()
   storageContainer : Node | null = null
   deactivate : Function | undefined = undefined
@@ -24,6 +26,7 @@ class KeepAliveImpl {
       return
     }
     const { hostCreateElement,moveElement } = currentComponentInstance.ctx.renderer
+    this.props = props
     this.storageContainer = hostCreateElement(`div`)
     this.deactivate = (vnode : Vnode) => {
       const component = vnode[VnodeTagAttr.COMPONENT]
@@ -43,26 +46,63 @@ class KeepAliveImpl {
     onMounted(cacheSubTree)
     onUpdated(cacheSubTree)
 
+    let currentVnode : Vnode | null = null
+    const { include,exclude,max } = this.props
+
     return () => {
       const { slots } = currentComponentInstance
       const vnode = slots[`default`]()
       if(!(isVnode(vnode) && vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT)) {
         return vnode
       }
+
       let { type,key } = vnode
       key = key ? key : type
+      const name = type.name
+      if(name && (
+        (include && !include.includes(name))
+        || 
+        (exclude && exclude.includes(name))
+      )) {
+        return vnode
+      }
+
+      const pruneCacheEntrt = () => {
+        currentVnode && resetShapFlag(currentVnode)
+        const key = this.keys.keys().next().value
+        this.cache.delete(key)
+        this.keys.delete(key)
+      }
+
       const cacheVnode = this.cache.get(key)
       if(cacheVnode) {
         vnode[VnodeTagAttr.COMPONENT] = cacheVnode[VnodeTagAttr.COMPONENT]
         vnode.shapeFlag |= ShapeFlags.COMPONENT_KEPT_ALIVE
+        this.keys.delete(key)
+        this.keys.add(key)
       } else {
         pendingCacheKey = key
-        vnode.shapeFlag |= ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE
+        if(max && max > this.cache.size) {
+          pruneCacheEntrt()
+        }
       }
+      vnode.shapeFlag |= ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE
+      currentVnode = vnode
       return vnode
     }
   }
 
+}
+
+function resetShapFlag(vnode : Vnode) {
+  let { shapeFlag } = vnode
+  if(shapeFlag & ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE) {
+    shapeFlag -= ShapeFlags.COMPONENT_SHOULD_KEEP_ALIVE
+  }
+  if(shapeFlag & ShapeFlags.COMPONENT_KEPT_ALIVE) {
+    shapeFlag -= ShapeFlags.COMPONENT_KEPT_ALIVE
+  }
+  vnode.shapeFlag = shapeFlag
 }
 
 function isKeepAlive(obj : object) {
